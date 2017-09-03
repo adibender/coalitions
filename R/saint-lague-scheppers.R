197206c9e328fbe2db8c7cf3befebd4faefc568d
adibender <bender.at.R@gmail.com>
2017-09-03 14:21:35 +0200

    Updated internal data (added .trans_df and .survey_sample). Simplified prettifying/renaming of party names/coalitions etc. (adresses part of #38)

--- R/saint-lague-scheppers.R
+++ R/saint-lague-scheppers.R
@@ -1,27 +1,27 @@
 #' Seat Distribution by Sainte-Lague/Scheppers
-#' 
-#' Calculates number of seats for the respective parties that have received more 
+#'
+#' Calculates number of seats for the respective parties that have received more
 #' than 5\% of votes (according to the method of Sainte-Lague/Schepers,
 #' see http://www.wahlrecht.de/verfahren/rangmasszahlen.html).
-#' 
+#'
 #' @inheritParams redistribute
 #' @param votes A numeric vector giving the redistributes votes
-#' @param parties A character vector indicating the names of parties with 
+#' @param parties A character vector indicating the names of parties with
 #' respective \code{votes}.
-#' @param n_seats The total number of seats that can be assigned to the different 
-#' parties. 
-#' @return A numeric vector giving the number of seats each party obtained. 
+#' @param n_seats The total number of seats that can be assigned to the different
+#' parties.
+#' @return A numeric vector giving the number of seats each party obtained.
 #' @import dplyr
 #' @importFrom reshape2 melt
 #' @seealso \code{\link{dHondt}}
 #' @export
 sls <- function(
   votes,
-  parties, 
+  parties,
   n_seats   = 598L) {
 
-  ## attention: div_mat is an internal object. see data_raw/internals.R
-  divisor.mat <- sum(votes)/t(div_mat[seq_along(votes), ]*votes)
+  ## attention: .div_mat is an internal object. see data_raw/internals.R
+  divisor.mat <- sum(votes)/t(.div_mat[seq_along(votes), ]*votes)
   colnames(divisor.mat) <- parties
 
   m.mat <- melt(divisor.mat, as.is=TRUE, value.name="seats")
@@ -30,7 +30,7 @@ sls <- function(
   # seat.mat <- bind_cols(rle.seats[2:1])
   # colnames(seat.mat) <- c("party","seats")
 
-  if( sum(rle.seats$length) != n_seats ) 
+  if( sum(rle.seats$length) != n_seats )
     stop(paste("Number of seats distributed not equal to", seats))
 
   rle.seats$length
@@ -39,7 +39,7 @@ sls <- function(
 
 
 # sls <- function(
-#   survey, 
+#   survey,
 #   seats   = 598,
 #   hurdle  = 0.05,
 #   epsilon = 10e-6,
@@ -58,9 +58,9 @@ sls <- function(
 #   seat.mat <- bind_cols(rle.seats[2:1])
 #   colnames(seat.mat) <- c("party","seats")
 
-#   if( nrow(seat.mat) != nrow(survey) ) 
+#   if( nrow(seat.mat) != nrow(survey) )
 #     stop ("Wrong number of parties after seat distribution")
-#   if( sum(seat.mat$seats) != seats ) 
+#   if( sum(seat.mat$seats) != seats )
 #     stop(paste("Number of seats distributed not equal to", seats))
 
 #   seat.mat
--- R/scrappers.R
+++ R/scrappers.R
@@ -1,6 +1,6 @@
 #' Sanitize character vectors containing percentages
-#' 
-#' @param vec Character vector containg percentages that can not be transformed 
+#'
+#' @param vec Character vector containg percentages that can not be transformed
 #' to numerics b/c of special characters
 #' @keywords internal
 sanitize_percent <- function(vec) {
@@ -14,18 +14,18 @@ sanitize_percent <- function(vec) {
 
 }
 
-#' Sanitze character vector 
-#' 
+#' Sanitze character vector
+#'
 #' Substitute all german "Umlaute"
-#' @param x A character vector. 
+#' @param x A character vector.
 #' @keywords internal
 sanitize_strings <- function(x) {
 
-	# lower case letters 
+	# lower case letters
 	x <- gsub("\u00f6", "oe", x)
 	x <- gsub("\u00fc", "ue", x)
 	x <- gsub("\u00e4", "ae", x)
-	# upper case letters 
+	# upper case letters
 	x <- gsub("\u00d6", "Oe", x)
 	x <- gsub("\u00dc", "Ue", x)
 	x <- gsub("\u00c4", "Ae", x)
@@ -35,7 +35,7 @@ sanitize_strings <- function(x) {
 }
 
 #' Extract numericals from string or character
-#' 
+#'
 #' Removes all characters that are not in [0-9].
 #' @param x A character vector.
 #' @param decimal Logical flag, indicating if x has a decimal separator
@@ -51,9 +51,9 @@ extract_num <- function(x, decimal=TRUE) {
 	as.numeric(gsub(replace_string, "", x))
 }
 
-#' Sanitize column names 
-#' 
-#' @param df A data frame with party names with special characters that need 
+#' Sanitize column names
+#'
+#' @param df A data frame with party names with special characters that need
 #' to be sanitized.
 #' @keywords internal
 sanitize_colnames <- function(df) {
@@ -81,8 +81,8 @@ sanitize_colnames <- function(df) {
 #' @importFrom stringr str_sub
 #' @export
 scrape_wahlrecht <- function(
-	address = "http://www.wahlrecht.de/umfragen/emnid.htm", 
-	parties = c("CDU", "SPD", "GRUENE", "FDP", "LINKE", "PIRATEN", "FW", "AFD", 
+	address = "http://www.wahlrecht.de/umfragen/emnid.htm",
+	parties = c("CDU", "SPD", "GRUENE", "FDP", "LINKE", "PIRATEN", "FW", "AFD",
 		"SONSTIGE")) {
 
 	atab <- read_html(address) %>%
@@ -92,10 +92,10 @@ scrape_wahlrecht <- function(
 	if(address == "http://www.wahlrecht.de/umfragen/politbarometer.htm") {
 		colnames(atab) <- atab[2, ]
 		ind.row.remove <- -1:-3
-	} else if (address == "http://www.wahlrecht.de/umfragen/gms.htm" | 
+	} else if (address == "http://www.wahlrecht.de/umfragen/gms.htm" |
 		address == "http://www.wahlrecht.de/umfragen/insa.htm") {
 		ind.row.remove <- -1:-4
-	} else { 
+	} else {
 		ind.row.remove <- -1:-3
 	}
 
@@ -109,7 +109,7 @@ scrape_wahlrecht <- function(
 	atab <- sanitize_colnames(atab)
 	parties <- colnames(atab)[colnames(atab) %in% tolower(parties)]
 	# transform percentage string to numerics
-	atab %<>% mutate_at(c(parties), extract_num) %>% 
+	atab %<>% mutate_at(c(parties), extract_num) %>%
 		mutate_at("befragte", extract_num, decimal=FALSE)
 
 	atab %<>% mutate(datum = dmy(datum))
@@ -117,9 +117,9 @@ scrape_wahlrecht <- function(
 		start = dmy(paste0(str_sub(zeitraum, 1, 6), str_sub(datum, 1, 4))),
 		end   = dmy(paste0(str_sub(zeitraum, 8, 13), str_sub(datum, 1, 4))))
 
-	atab %<>% 
-		mutate(total = rowSums(atab[, parties], na.rm=TRUE)) %>% 
-		filter(total==100, !is.na(befragte), !is.na(datum)) %>% 
+	atab %<>%
+		mutate(total = rowSums(atab[, parties], na.rm=TRUE)) %>%
+		filter(total==100, !is.na(befragte), !is.na(datum)) %>%
 		select(one_of(c("datum", "start", "end", parties, "befragte")))
 
 	return(atab)
@@ -129,15 +129,15 @@ scrape_wahlrecht <- function(
 
 
 #' Scrape surveys from all survey institutes
-#' 
+#'
 #' @import dplyr
 #' @importFrom purrr map
 #' @export
 get_surveys <- function() {
 
-	institutes_df %>% 
+	.institutes_df %>%
 		mutate(
-			surveys = map(address, scrape_wahlrecht), 
+			surveys = map(address, scrape_wahlrecht),
 			surveys = map(.x=surveys, collapse_parties)) %>%
 		select(-address)
 
@@ -145,28 +145,28 @@ get_surveys <- function() {
 
 
 #' Extract "meta" information from survey data base
-#' 
-#' @param surveys_df A data frame containing surveys from different survey 
-#' institutes as returned by \code{\link{get_surveys}}. 
+#'
+#' @param surveys_df A data frame containing surveys from different survey
+#' institutes as returned by \code{\link{get_surveys}}.
 #' @importFrom dplyr select
 #' @importFrom tidyr unnest
 #' @keywords internal
 #' @export
 get_meta <- function(surveys_df) {
 
-	surveys_df %>% 
-		unnest() %>% 
+	surveys_df %>%
+		unnest() %>%
 		select(institute, datum:befragte)
 
 }
 
 
 #' Transform surveys in long format
-#' 
-#' Given a data frame containing multiple surveys (one row per survey), transforms 
-#' the data into long format with one row per party. 
+#'
+#' Given a data frame containing multiple surveys (one row per survey), transforms
+#' the data into long format with one row per party.
 #' @inheritParams scrape_wahlrecht
-#' @param surveys A data frame with one survey per row.  
+#' @param surveys A data frame with one survey per row.
 #' @import checkmate magrittr dplyr
 #' @importFrom tidyr gather nest
 #' @importFrom purrr compose
@@ -176,8 +176,8 @@ get_meta <- function(surveys_df) {
 #' emnid.long <- collapse_parties(emnid)
 #' @export
 collapse_parties <- function(
-	surveys, 
-	parties = c("cdu", "spd", "gruene", "fdp", "linke", "piraten", "fw", "afd", 
+	surveys,
+	parties = c("cdu", "spd", "gruene", "fdp", "linke", "piraten", "fw", "afd",
 		"sonstige")) {
 
 	assert_data_frame(surveys, min.rows=1, min.cols=3)
@@ -185,11 +185,37 @@ collapse_parties <- function(
 
 	surveys %<>% select_if(compose("!", all, is.na))
 	av.parties <- colnames(surveys)[colnames(surveys) %in% parties]
-	surveys <- gather(surveys, party, percent, select_vars(names(surveys),one_of(av.parties))) %>% 
+	surveys <- gather(surveys, party, percent, select_vars(names(surveys),one_of(av.parties))) %>%
 		arrange(desc(datum))
 
-	surveys %>% mutate(votes = percent/100 * befragte) %>% 
-		filter(!is.na(percent)) %>% 
+	surveys %>% mutate(votes = percent/100 * befragte) %>%
+		filter(!is.na(percent)) %>%
 	  as_tibble() %>%
 		nest(party:votes, .key="survey")
+}
+
+
+#' Extract latest survey
+#'
+#' Given a specific date, extract the survey from this date or the last one
+#' before this date.
+#'
+#' @rdname get_surveys
+#' @param surveys If provided, lates survey will be obtained from this object,
+#' otherwise calls \code{\link{get_surveys}}.
+#' @param date Specifies the date, relative to which latest survey will
+#' be searched for. Defaults to \code{Sys.Date}.
+#' @importFrom tidyr unnest
+#' @importFrom dplyr filter
+get_latest <- function(surveys=NULL, date = Sys.Date()) {
+
+	if(is.null(surveys)) {
+		surveys <- get_surveys()
+	}
+
+	surveys %>%
+		unnest() %>%
+		filter(datum <= as.Date(date)) %>%
+		filter(datum == max(datum))
+
 }
